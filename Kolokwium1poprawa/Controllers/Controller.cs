22a32f98c2dfd9c5b2dcf8bcaa2949769c921837
s30679 <s30679@pjwstk.edu.pl>
2025-06-16 17:40:28 +0200

    template

--- Kolokwium1poprawa/Controllers/Controller.cs
+++ Kolokwium1poprawa/Controllers/Controller.cs
@@ -1,6 +1,39 @@
-﻿namespace Kolokwium1poprawa.Controllers;
+﻿using Kolokwium1poprawa.Repositories;
+using Kolokwium1poprawa.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class Controller
+namespace Kolokwium1poprawa.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class Controller : ControllerBase
 {
-    
+    private readonly IService _service;
+
+    public Controller(IService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAsync(CancellationToken cancellationToken, int id)
+    {
+        var pole=null;
+        if (pole == null)
+        {
+            return NotFound();
+        }
+        return Ok();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddAsync(CancellationToken cancellationToken)
+    {
+        var pole=null;
+        if (pole == null)
+        {
+            return BadRequest();
+        }
+        return Created();
+    }
 }
